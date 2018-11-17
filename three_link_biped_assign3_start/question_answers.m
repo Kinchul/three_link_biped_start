d77641f62029e054ea2ea29a760efbcd1cf62c0e
Victor Delafontaine <vicdelafont@gmail.com>
2018-11-17 10:32:11 +0100

    misc

--- three_link_biped_assign3_start/question_answers.m
+++
@@ -1,7 +0,0 @@
-% % Q1: Note that the visualize.m function has an extra input r0 which is the 
-% % position of the stance foot in the global frame. Why do we need this?
-
-% % Q2: Calculate the real time factor as defined in the animate.m script. 
-% % What does a real time factor of 1 mean? 
-% % How about a real time factor smaller than 1?
-% % How does 'skip' effect this value?
